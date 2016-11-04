a2b81827e07fcb99f65749217321d6b0c04dbed4
Anthony Pennacchio <mrpennacchio@gmail.com>
2016-11-04 15:22:05 -0400

    added two models: question and answer

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: false )}
+let(:answer) { Answer.create!(body: "New Answer Title", question: question) }
+
+  describe "attributes" do
+    it "has a body" do
+      expect(answer).to have_attributes(body: "New Answer Title")
+    end
+
+    it "should respond to body" do
+      expect(answer).to respond_to(:body)
+    end
+  end
+end
---
+++ spec/models/question_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: false )}
+
+  describe "attributes" do
+    it "has title, body, and resolved attributes" do
+      expect(question).to have_attributes(title: "New Question Title", body: "New Question Body", resolved: false)
+    end
+
+    it "responds to title" do
+      expect(question).to respond_to(:title)
+    end
+
+    it "responds to body" do
+      expect(question).to respond_to(:body)
+    end
+
+    it "responds to resolved" do
+      expect(question).to respond_to(:resolved)
+    end
+  end
+end
