c05cec627fc9aaef587a66ead15419b5f2d29621
Anthony Pennacchio <mrpennacchio@gmail.com>
2016-11-06 10:26:29 -0500

    added Create and Read components

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,33 @@ class PostsController < ApplicationController
   end
 
   def show
+    # => find the post that corresponds to the id in the params that was passed to show and assign it to @post
+    #    only assigning it to a single post. 
+    @post = Post.find(params[:id])
   end
 
   def new
+    # => create an instance variable @post, assign it to an empty post by Post.new
+    @post = Post.new
   end
+
+  def create
+    # => call Post.new to create new instance of Post
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    # => if we save Post to the database a display success message wil appear
+    if @post.save
+      # => assign value to flash[:notice]. this provides a way to pass temporary values between actions
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      # => if not saved, displace error message and render a new view again
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+  end
+end
 
   def edit
   end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -6,31 +6,77 @@ RSpec.describe PostsController, type: :controller do
   let(:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
   describe "GET #index" do
+      it "returns http success" do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+      # expect the index to return an array of one item.
+      it "assigns [my_post] to @posts" do
+        get :index
+        # => use assigns methd to give access to instance variables assigned in the action that are available for the view
+        expect(assigns(:posts)).to eq([my_post])
+      end
+  end
+
+  # => when :new is invoked, a new and unsaved post object is created. when create is invoke, newly created object is presisted to the database
+  #    this mimics the RESTful parts of HTTP. :new does not generate new data
+  describe "GET new" do
+      it "returns http success" do
+          get :new
+          expect(response).to have_http_status(:success)
+      end
+      # => we expect PostsController#new to render the posts new view...with render_template
+      it "renders the #new view" do
+          get :new
+          expect(response).to render_template :new
+      end
+      # => we expect @post instance variable to be initialized by PostsController#new.
+      #    assigns gives us access to the @post variable, and assigns @post to :post
+      it "instantiates @post" do
+          get :new
+          expect(assigns(:post)).not_to be_nil
+      end
+  end
+
+
+  describe "POST create" do
+      # => after PostsController#create is called, Post instances will increase by 1
+      it "increases the number of Post by 1" do
+          expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
+      end
+      # => when create is POSTed to, we expect newly created post to be assigned to @post
+      it "assigns the new post to @post" do
+          post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+          expect(assigns(:post)).to eq Post.last
+      end
+      # => expect to he be redirected to the newly created post
+      it "redirects to the new post" do
+          post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+          expect(response).to redirect_to Post.last
+      end
+  end
+
+
+  describe "GET show" do
     it "returns http success" do
-      get :index
+      # => pass {id: my_post.id} to show as a parameter with params has
+      get :show, {id: my_post.id}
       expect(response).to have_http_status(:success)
     end
-    # expect the index to return an array of one item.
-    it "assigns [my_post] to @posts" do
-      get :index
-      # => use assigns methd to give access to instance variables assigned in the action that are available for the view
-      expect(assigns(:posts)).to eq([my_post])
+    # => expect response to return the show view using render_template matcher
+    it "renders the #show view" do
+
+      get :show, {id: my_post.id}
+      expect(response).to render_template :show
+    end
+
+    it "assigns my_post to @post" do
+      get :show, {id: my_post.id}
+      # => expect post to equal my_post because we call show with id of my_post
+      expect(assigns(:post)).to eq(my_post)
     end
   end
 
-  # describe "GET #show" do
-  #   it "returns http success" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  #
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
   #
   # describe "GET #edit" do
   #   it "returns http success" do
