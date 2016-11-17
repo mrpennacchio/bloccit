b25bee06681d75777dfe8f4bbcf6c0be94b7a519
Anthony Pennacchio <mrpennacchio@gmail.com>
2016-11-17 13:28:39 -0500

    added sign-in feature--need to pass session tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
     # we save the user to the database. If the database save is successful, add flash message, redirect to root path
     if @user.save
       flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+      create_session(@user)
       redirect_to root_path
     else
       flash.now[:alert] = "There was an error creating your account. Please try again"
