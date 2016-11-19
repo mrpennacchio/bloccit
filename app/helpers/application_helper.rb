72ee3b0c320d438d45b8c82b275232b428e74cfd
Anthony Pennacchio <mrpennacchio@gmail.com>
2016-11-19 10:54:31 -0500

    added gravatar next to current user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
     # => used to build the html and css to dosplay the form element and any associated errors
     content_tag :div, capture(&block), class: css_class
   end
+
+  def avatar_url(user)
+      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
 end
