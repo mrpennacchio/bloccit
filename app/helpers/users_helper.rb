c6f0a280a8701e3560f45882e0e318f82680353a
Anthony Pennacchio <mrpennacchio@gmail.com>
2016-12-05 10:27:55 -0500

    cleaned up helper and view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,12 +2,10 @@ module UsersHelper
 
   def has_no_posts?
     @user.posts.count == 0
-    "{user.name} has not submitted any posts yet."
   end
 
   def has_no_comments?
     @user.comments.count == 0
-    "{user.name} has not submitted any posts yet."
   end
 
 end
